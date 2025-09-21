4815d85ad783973b686304ecf6ca697486c38024
Taís campos dos Santos <88733561+Taiscs@users.noreply.github.com>
2025-09-21 17:46:35 -0300

    Add files via upload

--- app/Models/Venda.php
+++ app/Models/Venda.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Venda extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'total' => 'float',
+        'desconto' => 'float',
+        'acrescimo' => 'float',
+        'pacotes_valor_manual' => 'float',
+    ];
 }
