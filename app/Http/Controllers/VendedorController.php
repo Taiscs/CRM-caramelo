e8c4fc86fd48869efbd45ffd028eb1172351d006
Taís campos dos Santos <88733561+Taiscs@users.noreply.github.com>
2025-09-24 03:30:07 -0300

    Add files via upload

--- app/Http/Controllers/VendedorController.php
+++ app/Http/Controllers/VendedorController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Consultor;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class VendedorController extends Controller
 {
     // Mostra a página de cadastro
     public function create()
     {
-        $unidades = \DB::table('unidade')->get();
+        $unidades = DB::table('unidade')->get();
         $consultores = Consultor::all();
 
         return view('vendedor.create', compact('unidades', 'consultores'));
@@ -20,14 +22,16 @@ class VendedorController extends Controller
     public function store(Request $request)
     {
         $data = $request->only(['nome_consultor', 'sobrenome_consultor', 'unidade', 'ativo']);
+        $data['foto'] = null; // Inicializa com null
 
-if ($request->hasFile('foto')) {
-    $file = $request->file('foto');
-    $filename = time() . '_' . $file->getClientOriginalName();
-    // Salva na pasta correta
-    $file->move(public_path('storage/fotos_vendedores'), $filename);
-    $data['Foto'] = 'storage/fotos_vendedores/' . $filename;
-}
+        if ($request->hasFile('foto')) {
+            $file = $request->file('foto');
+            // Remove espaços do nome do arquivo para evitar problemas
+            $filename = time() . '_' . preg_replace('/\s+/', '_', $file->getClientOriginalName());
+            // Salva na pasta correta
+            $file->move(public_path('storage/fotos_vendedores'), $filename);
+            $data['foto'] = 'storage/fotos_vendedores/' . $filename;
+        }
 
         Consultor::create($data);
 
@@ -38,15 +42,22 @@ if ($request->hasFile('foto')) {
     public function update(Request $request, $id)
     {
         $consultor = Consultor::findOrFail($id);
-
         $data = $request->only(['nome_consultor', 'sobrenome_consultor', 'unidade', 'ativo']);
 
-    if ($request->hasFile('foto')) {
-    $file = $request->file('foto');
-    $filename = time() . '_' . preg_replace('/\s+/', '_', $file->getClientOriginalName());
-    $file->move(public_path('storage/fotos_vendedores'), $filename);
-    $data['Foto'] = 'storage/fotos_vendedores/' . $filename;
-}
+        if ($request->hasFile('foto')) {
+            // Deleta a foto antiga se ela existir e for diferente do padrão
+            if ($consultor->foto && strpos($consultor->foto, 'default-avatar.png') === false) {
+                $caminhoAntigo = public_path($consultor->foto);
+                if (file_exists($caminhoAntigo)) {
+                    unlink($caminhoAntigo);
+                }
+            }
+
+            $file = $request->file('foto');
+            $filename = time() . '_' . preg_replace('/\s+/', '_', $file->getClientOriginalName());
+            $file->move(public_path('storage/fotos_vendedores'), $filename);
+            $data['foto'] = 'storage/fotos_vendedores/' . $filename;
+        }
 
 
         $consultor->update($data);
