bc1e21dcf2320ac5f82da6ed7f1a70a51a06514d
Taís campos dos Santos <88733561+Taiscs@users.noreply.github.com>
2025-09-25 17:43:34 -0300

    Add files via upload

---
+++ app/Models/Oportunidade.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Oportunidade extends Model
+{
+    use HasFactory;
+
+    /**
+     * Define o nome da tabela no banco de dados.
+     * @var string
+     */
+    protected $table = 'oportunidades';
+    
+    /**
+     * Define os campos que podem ser preenchidos em massa.
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'vendedor_id',
+        'cliente_id',
+        'descricao_oportunidade',
+        'data_oportunidade',
+    ];
+}
